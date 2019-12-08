ecafdcb92629363f23a5544753745ff86c9348e6
Richmond Chang <changrichmond@gmail.com>
2019-12-07 20:32:18 -0800

    Exercise 05 - Add a box trigger beneath the camera check for game over

---
+++ Assets/Scripts/GameOverChecker.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverChecker : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        gameOver();
+    }
+
+    private void gameOver() {
+        SceneManager.LoadScene("99-game-over");
+    }
+}
