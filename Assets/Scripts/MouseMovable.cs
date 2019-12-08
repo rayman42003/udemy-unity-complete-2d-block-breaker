d3b76a4da18d7d2a153b1cbc9af1b33340c57a4b
Richmond Chang <changrichmond@gmail.com>
2019-12-07 23:32:43 -0800

    Exercise 05 - Make paddle move with the mouse

---
+++ Assets/Scripts/MouseMovable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MouseMovable : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+
+    private void Start() {
+    }
+
+    private void Update() {
+        var widthInWorldUnits = mainCamera.orthographicSize * 8 / 3;
+        var mouseXPosInWorldUnits = Input.mousePosition.x / Screen.width * widthInWorldUnits;
+        Vector2 paddlePos = new Vector2(mouseXPosInWorldUnits, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
