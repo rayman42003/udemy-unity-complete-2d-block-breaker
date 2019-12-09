f3b2f3ff11f131fdac0194c8ceef09faa09bbfb1
Richmond Chang <changrichmond@gmail.com>
2019-12-08 18:02:00 -0800

    Exercise 05 - Add script to have block take damage and be destroyed, also make block + collider bigger

---
+++ Assets/Scripts/Damagable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Damagable : MonoBehaviour
+{
+    [SerializeField]
+    private int hitPoints = 1;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        damage();
+    }
+
+    private void damage() {
+        hitPoints--;
+        if (hitPoints == 0) {
+            kill();
+        }
+    }
+
+    private void kill() {
+        Destroy(gameObject);
+    }
+}
