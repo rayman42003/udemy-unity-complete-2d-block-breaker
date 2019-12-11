ec3cf1d867772b9a93f2b6fea74b01649c8b7529
Richmond Chang <changrichmond@gmail.com>
2019-12-11 09:21:25 -0800

    Exercise 05 - Make it so ball can make noise by making ball collidable and using the even to trigger playing of audio sound

---
+++ Assets/Scripts/Audible.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Audible : MonoBehaviour
+{
+    private AudioSource myAudioSource;
+
+    public void playSound() {
+        myAudioSource.Play();
+    }
+
+    private void Start() {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+}
---
+++ Assets/Scripts/Collidable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Collidable : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent onCollide = new UnityEvent();
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        onCollide.Invoke();
+    }
+}
