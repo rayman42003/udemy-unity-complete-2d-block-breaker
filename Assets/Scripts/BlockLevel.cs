a76cce04279d4d2a2a42fc487fed0c73ad2fe7ee
Richmond Chang <changrichmond@gmail.com>
2020-01-06 10:08:50 -0800

    Exercise 05 - Add BlockLevel to keep track of how many blocks are created and destroyed, using the Destroyable's onDestroy event, as well as creating a Spawnable for an onCreate event:     -  TODO: Figure out how to not have every block have to be wired to the block level, which requires adding a reference to each block in the script and to have it be set to call add/remove block (could easily cause bugs in a game if someone forgot to do this)

---
+++ Assets/Scripts/BlockLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BlockLevel : MonoBehaviour
+{
+    [SerializeField]
+    private int numBlocks = 0;
+
+    public void addBlock() {
+        numBlocks++;
+    }
+
+    public void removeBlock() {
+        numBlocks--;
+    }
+}
---
+++ Assets/Scripts/Spawnable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Spawnable : MonoBehaviour
+{
+    [SerializeField]
+    public UnityEvent onCreate;
+
+    private void Start() {
+        onCreate.Invoke();
+    }
+}
