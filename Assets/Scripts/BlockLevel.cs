4c68033973f769fe801a5d2be0dfb1a0bf1b855a
Richmond Chang <changrichmond@gmail.com>
2020-03-30 15:31:55 -0700

    Exercise 05 - Add method of changing game speed

--- Assets/Scripts/BlockLevel.cs
+++ Assets/Scripts/BlockLevel.cs
@@ -3,8 +3,12 @@ using UnityEngine.Events;
 
 public class BlockLevel : MonoBehaviour
 {
+    [Range(0.1f, 2.0f)]
     [SerializeField]
     private int numBlocks = 0;
+
+    [SerializeField]
+    private float gameSpeed = 0.5f;
 
     [SerializeField]
     private UnityEvent onLevelCleared = new UnityEvent();
@@ -18,5 +22,9 @@ public class BlockLevel : MonoBehaviour
         if (numBlocks <= 0) {
             onLevelCleared.Invoke();
         }
+    }
+
+    private void Update() {
+        Time.timeScale = gameSpeed;
     }
 }
