147a4206555e7d8384ad9bb6d917b1a47ca9010d
Richmond Chang <changrichmond@gmail.com>
2021-01-10 19:36:02 -0800

    Fix bug with multiple balls where the ball would occasionally lose too much speed by occasionally clamping its velocity back into a range

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private float minVelocityMagnitude = 6.5f;
+
+    [SerializeField]
+    private float maxVelocityMagnitude = 8.0f;
+
+    [SerializeField]
+    private float recalibrationPeriod = 1.0f;
+
+    [SerializeField]
+    private bool shouldRecalibrate = false;
+
+    private void Start() {
+        StartCoroutine(recalibrationCooldown(recalibrationPeriod));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        float currVelocityMagnitude = rigidbody.velocity.magnitude;
+        if (currVelocityMagnitude != 0 && shouldRecalibrate) {
+            float clampedVelocityMagnitude = Mathf.Clamp(currVelocityMagnitude, minVelocityMagnitude, maxVelocityMagnitude);
+            float scale = clampedVelocityMagnitude / currVelocityMagnitude;
+
+            rigidbody.simulated = false;
+            rigidbody.velocity *= scale;
+            rigidbody.simulated = true;
+
+            shouldRecalibrate = false;
+            StartCoroutine(recalibrationCooldown(recalibrationPeriod));
+        }
+    }
+
+    private IEnumerator recalibrationCooldown(float time) {
+        yield return new WaitForSeconds(time);
+        shouldRecalibrate = true;
     }
 }
